3065c60bed7156a1813905d467a9524b3ffe4986
Kmberry1989 <kylematthewberry@gmail.com>
2025-06-16 01:41:10 -0400

    aha

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Important for React components
+    "./public/index.html",      // Important for the main HTML file
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
